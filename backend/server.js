9f05c38b4d2d78bbcd7da67ab2dec184be041eee
“akiverin” <kiverin03@yandex.ru>
2024-05-06 16:15:29 +0300

    add login, logout, getme controllers, routes, protect route meddleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
